0055d0744bd7ecdd80c1a1ef59398d0f7dec192b
xsami <elsam1702@gmail.com>
2018-04-12 09:06:59 -0400

    Adding git ignore file and fixing username

--- logInfo.bat
+++ logInfo.bat
@@ -1,5 +1,5 @@
 @echo off
-@cd c:\users\Samil.Vargas\Desktop\
+@cd c:\users\%USERNAME%\Desktop\
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 @echo Some one entered on the computer "at" %mydate%_%mytime%  >> info.txt
